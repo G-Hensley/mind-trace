7d3c4b7fe4c2d0bf4db7a20e163cd921637538c1
Gavin Hensley <gavinhensley@protonmail.com>
2025-09-07 00:00:00 -0400

    Create index.ts validation file to export all validators

---
+++ back-end/src/shared/validation/index.ts
@@ -0,0 +1,7 @@
+export * from './base';
+export * from './profile';
+export * from './utils';
+export * from './organization';
+export * from './student';
+export * from './behaviorLog';
+export * from './auth';
